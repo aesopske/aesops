f789ef13fef0685e0aa1e21e11be6511b7559057
Newton Mbugua <mymbugua@gmail.com>
2021-12-15 14:06:26 +0300

    bug: fix for rss writefile issue

--- src/utils/rssfeed.js
+++ src/utils/rssfeed.js
@@ -59,8 +59,15 @@ export async function generateRssFeed() {
         })
     }
 
-    fs.mkdirSync('public/rss', { recursive: true })
-    fs.writeFileSync('public/rss/feed.xml', feed.rss2())
-    fs.writeFileSync('public/rss/atom.xml', feed.atom1())
-    fs.writeFileSync('public/rss/json.xml', feed.json1())
+    if (process.env.NODE_ENV === 'production') {
+        // fs.mkdirSync('/rss', { recursive: true })
+        fs.writeFileSync('/rss/feed.xml', feed.rss2())
+        fs.writeFileSync('/rss/atom.xml', feed.atom1())
+        fs.writeFileSync('/rss/json.xml', feed.json1())
+    } else {
+        fs.mkdirSync('public/rss', { recursive: true })
+        fs.writeFileSync('public/rss/feed.xml', feed.rss2())
+        fs.writeFileSync('public/rss/atom.xml', feed.atom1())
+        fs.writeFileSync('public/rss/json.xml', feed.json1())
+    }
 }
