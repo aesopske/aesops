6feca117a5fcded460985c23457f18a2bc32d6a8
Newton Mbugua <newton.mbugua@adi.do>
2024-05-01 14:48:12 +0100

    feat: added topbar loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import '@app/styles/global.css'
 import '@fontsource-variable/lora'
 import '@fontsource-variable/bricolage-grotesque'
+
+import TopLoader from 'nextjs-toploader'
+
 import Footer from '@/components/common/Footer'
 import NewNavbar from '@components/common/NewNavbar'
 
@@ -8,6 +11,7 @@ function RootLayout({ children }) {
     return (
         <html lang='en'>
             <body className='bg-gradient-t-b from-aes-light to-aes-primary'>
+                <TopLoader color='#15616D' showSpinner={false} />
                 <NewNavbar />
                 <main>{children}</main>
                 <Footer />
