b83e26157de3a3e43bc7535a932c33ffc805cbbc
Newton Mbugua <newton.mbugua@adi.do>
2024-05-01 14:49:44 +0100

    feat: added open ai api key to env ts file

--- src/env.ts
+++ src/env.ts
@@ -4,10 +4,12 @@ import { createEnv } from '@t3-oss/env-nextjs'
 export const env = createEnv({
     server: {
         HUGGING_FACE_ACCESS_TOKEN: z.string(),
+        OPENAI_API_KEY: z.string(),
     },
     client: {},
 
     runtimeEnv: {
         HUGGING_FACE_ACCESS_TOKEN: process.env.HUGGING_FACE_ACCESS_TOKEN,
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     },
 })
