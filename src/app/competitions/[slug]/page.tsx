cbe0c59e2436ffd8800c9701b1e169af191ecb36
Newton Mbugua <mymbugua@gmail.com>
2024-07-22 21:15:50 +0100

    fix: update default open tab

--- src/app/competitions/[slug]/page.tsx
+++ src/app/competitions/[slug]/page.tsx
@@ -118,7 +118,7 @@ async function page({ params }: { params: QueryParams }) {
                 <hr className='my-4' />
                 <div>
                     <Tabs
-                        defaultValue='overview'
+                        defaultValue={competition?.tabs?.[0]?.title.toLowerCase()}
                         className='w-full max-w-3xl rounded-md'>
                         <TabsList className='bg-brandaccent-50/80 py-6 px-2 rounded-full shadow-sm'>
                             <ListWrapper list={competition.tabs ?? []}>
